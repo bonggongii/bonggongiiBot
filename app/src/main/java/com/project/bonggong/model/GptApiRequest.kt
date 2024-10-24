cd4165d30d40d2e5e752ada6b74bf24e6efac4cb
HJunng <guswjd1gh@gmail.com>
2024-10-24 14:55:04 +0900

    [refactor] gpt api 활용 전, 더미 메세지 출력할 수 있도록 구조 구축

--- app/src/main/java/com/project/bonggong/model/GptApiRequest.kt
+++ app/src/main/java/com/project/bonggong/model/GptApiRequest.kt
@@ -1,16 +1,21 @@
 package com.project.bonggong.model
 
 import com.project.bonggong.ChatContract
+import com.project.bonggong.R
 
 class GptApiRequest: ChatContract.Model{
 
     // GPT API 요청
     override fun sendGptRequest(
         input: String, // 입력 데이터
-        callback: (Message) -> Unit, // 성공 응답 콜백 함수
+        callback: (String) -> Unit, // 성공 응답 콜백 함수
         errorCallback: (Throwable) -> Unit // 실패 응답 콜백 함수
     ) {
-        TODO("Not yet implemented")
+        // todo : gpt api 요청 건으로 변경
+
+        // 임시 고정된 문자열 반환
+        val temporaryMessage = "안녕 나는 GPT야. 뭘 도와줄까?"
+        callback(temporaryMessage)
 
     }
 }
