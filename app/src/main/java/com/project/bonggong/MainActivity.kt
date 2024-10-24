079ac15d21e9d6df89155d4c4a91303618cf8bb0
HJunng <guswjd1gh@gmail.com>
2024-10-24 15:00:50 +0900

    [chore] ChatActivity 위치 이동

--- app/src/main/java/com/project/bonggong/MainActivity.kt
+++ app/src/main/java/com/project/bonggong/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.project.bonggong.view.ChatActivity
 
 class MainActivity : AppCompatActivity() {
 
