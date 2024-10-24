40c7c487bad6266e29b2a146c1c798d11d100f56
HJunng <guswjd1gh@gmail.com>
2024-10-24 14:57:23 +0900

    [refactor] Contract.View 상속 및 Presenter와 연결

--- app/src/main/java/com/project/bonggong/view/ChatActivity.kt
+++ app/src/main/java/com/project/bonggong/view/ChatActivity.kt
@@ -1,21 +1,28 @@
-package com.project.bonggong
+package com.project.bonggong.view
 
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.project.bonggong.ChatContract
+import com.project.bonggong.MessageAdapter
+import com.project.bonggong.R
+import com.project.bonggong.model.GptApiRequest
 import com.project.bonggong.model.Message
+import com.project.bonggong.presenter.ChatPresenter
 
-class ChatActivity : AppCompatActivity() {
+class ChatActivity : AppCompatActivity(), ChatContract.View {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var messageInput: EditText
     private lateinit var sendButton: ImageButton
     private lateinit var adapter: MessageAdapter
+    private lateinit var presenter: ChatContract.Presenter
 
     // 메시지를 저장할 리스트 (Message 객체로)
     private val messages = mutableListOf<Message>()
@@ -23,6 +30,9 @@ class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        // Presenter 초기화 (GptApiRequest를 Model로 사용)
+        presenter = ChatPresenter(this, GptApiRequest())
 
         // View 초기화, 컴포넌트를 XML에서 가져오기
         recyclerView = findViewById(R.id.recyclerView)
@@ -47,12 +57,25 @@ class ChatActivity : AppCompatActivity() {
                 recyclerView.scrollToPosition(messages.size - 1)
                 messageInput.text.clear()
 
-                // 챗봇 메시지 추가
-                val chatbotMessage = Message("챗봇의 응답", R.drawable.bonggong_profile,  false)
-                messages.add(chatbotMessage)
-                adapter.notifyItemInserted(messages.size - 1)
-                recyclerView.scrollToPosition(messages.size - 1)
+
             }
         }
+    }
+
+    override fun showLoading() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideLoading() {
+        TODO("Not yet implemented")
+    }
+
+    override fun displayGPTResponse(response: Message) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showError(errorMessage: String) {
+        // todo
+        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
     }
 }
